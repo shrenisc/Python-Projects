c8881bcc04adf005efe2b4c3c02b401c592d1844
shsh-1507 <91794581+shsh-1507@users.noreply.github.com>
2021-10-17 13:04:59 +0530

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -7,6 +7,11 @@ if guess < 5:
         print('you guessed it!'.upper())
     else:
         print("Sorry you haven't guessed correctly")
+        retry = input('Do you want to try again?(Y/N)')
+        if str(retry.upper()) == 'Y':
+            print('Coming Soon')
+        elif str(retry.upper()) == 'N':
+            print('Good choice')
 elif guess > 5:
     print('Guess lower')
     guess = int(input())
@@ -14,5 +19,10 @@ elif guess > 5:
         print('you guessed it!'.upper())
     else:
         print("Sorry you haven't guessed correctly")
+        retry = input('Do you want to try again?(Y/N)')
+        if str(retry.upper()) == 'Y':
+            print('Coming Soon')
+        elif str(retry.upper()) == 'N':
+            print('Good choice')
 else:
     print('You guessed it!'.upper())
